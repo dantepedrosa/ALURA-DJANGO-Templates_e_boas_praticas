88f43185f76519540e7574c6378c759feb5bf108
Dante Pedrosa <dantepedrosa@gmail.com>
2023-10-25 11:35:21 -0300

    imagem.html pronta

--- galeria/views.py
+++ galeria/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galeria/index.html')
+
+def imagem(request):
+    # TODO - Criar imagem 
+    return render(request, 'galeria/imagem.html')
