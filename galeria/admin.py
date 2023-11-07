ef654f73a1e4cf15382ed8ae3552df8e16c424cb
Dante Pedrosa <dantepedrosa@gmail.com>
2023-11-06 22:47:43 -0300

    Implemented "publicada"

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "categoria", "legenda")
+    list_display = ("id", "nome", "categoria", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
-    list_per_page = 4
+    list_editable = ("publicada",)
+    list_per_page = 4    
 
 admin.site.register(Fotografia, ListandoFotografias)
