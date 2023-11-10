a1856116bea083334493473a5dec86df571511ea
Dante Pedrosa <dantepedrosa@gmail.com>
2023-11-10 12:00:31 -0300

    App usuarios - urls e views

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from usuarios.views import login, cadastro
 
 urlpatterns = [
-    # TODO Create urls
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
+    # TODO - Create logout page: path('logout', logout, name='logout'),
 ]
