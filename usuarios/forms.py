473d5a3582d7e776d9e64768139de53db8e8e7ea
Dante Pedrosa <dantepedrosa@gmail.com>
2023-11-10 23:30:45 -0300

    Formulário de cadastro implementado em Django

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,49 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome completo",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: joaosilva@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label="Confirmação de senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
