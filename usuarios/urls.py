dc0d0f601f71f3bd7f43c4dfe08c9f3040b9162b
Dante Pedrosa <dantepedrosa@gmail.com>
2024-01-08 19:46:45 -0300

    Sistema logout criado

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from usuarios.views import login, cadastro
+from usuarios.views import login, cadastro, logout
 
 urlpatterns = [
     path('login', login, name='login'),
     path('cadastro', cadastro, name='cadastro'),
-    # TODO - Create logout page: path('logout', logout, name='logout'),
+    path('logout', logout, name='logout'),
 ]
