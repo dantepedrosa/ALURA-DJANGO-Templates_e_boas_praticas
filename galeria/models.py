e0eae7cd1ef837b7975184a511b0c07c3c61fd4d
Dante Pedrosa <dantepedrosa@gmail.com>
2024-01-29 11:42:14 -0300

    Refactor models.py, style.css, cadastro.html, forms.py, and views.py

--- galeria/models.py
+++ galeria/models.py
@@ -18,7 +18,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     publicada = models.BooleanField(default=False)
-    usuario = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=False, related_name="user")
+    usuario = models.ForeignKey(
+        to=User, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=False, 
+        related_name="user",
+    )
 
     def __str__(self):
         return self.nome
